e0fb5ffb1fd243305409710c18c4ee1cbd56063d
Kristijan <kculina@protonmail.ch>
2022-08-09 12:47:48 +0200

    Add constraint to Name property of Location

--- Staycation/Staycation.Api/Models/Location.cs
+++ Staycation/Staycation.Api/Models/Location.cs
@@ -1,10 +1,12 @@
 ﻿using Staycation.Api.Data.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Staycation.Api.Models
 {
     public class Location
     {
         public int Id { get; set; }
+        [MaxLength(150)]
         public string Name { get; set; }
         public int PostalCode { get; set; }
 
