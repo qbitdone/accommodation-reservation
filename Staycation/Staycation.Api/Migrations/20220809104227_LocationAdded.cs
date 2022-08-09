8af8c7c1babfd4db680b05fb357a9a5a80bc01fa
Kristijan <kculina@protonmail.ch>
2022-08-09 12:45:04 +0200

    Add Location model and add migration

---
+++ Staycation/Staycation.Api/Migrations/20220809104227_LocationAdded.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Staycation.Api.Migrations
+{
+    public partial class LocationAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LocationId",
+                table: "Accommodations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostalCode = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accommodations_LocationId",
+                table: "Accommodations",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accommodations_Location_LocationId",
+                table: "Accommodations",
+                column: "LocationId",
+                principalTable: "Location",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accommodations_Location_LocationId",
+                table: "Accommodations");
+
+            migrationBuilder.DropTable(
+                name: "Location");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Accommodations_LocationId",
+                table: "Accommodations");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "Accommodations");
+        }
+    }
+}
---
+++ Staycation/Staycation.Api/Models/Location.cs
@@ -0,0 +1,13 @@
+﻿using Staycation.Api.Data.Models;
+
+namespace Staycation.Api.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PostalCode { get; set; }
+
+        public List<Accommodation> Accommodations { get; set; }
+    }
+}
