4c0b352f7655dd1a62b4686c59e84326ba7fb01b
Kristijan <kculina@protonmail.ch>
2022-08-09 12:52:47 +0200

    Create LocationService

---
+++ Staycation/Staycation.Api/Services/LocationService.cs
@@ -0,0 +1,13 @@
+﻿using Staycation.Api.DatabaseContext;
+
+namespace Staycation.Api.Services
+{
+    public class LocationService
+    {
+        private AppDbContext _context;
+        public LocationService (AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
