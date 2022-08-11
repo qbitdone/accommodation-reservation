d894237d548507e198e1f1e0e71c9bde1df71ff1
Kristijan <kculina@protonmail.ch>
2022-08-11 15:44:32 +0200

    Feat: [Z-2] Update LocationResponse model

--- Staycation/Staycation.Api/Models/LocationResponse.cs
+++ Staycation/Staycation.Api/Models/LocationResponse.cs
@@ -3,8 +3,8 @@
     public class LocationResponse
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-
+        public string ImageUrl { get; set; }
         public int PostalCode { get; set; }
+        public string Name { get; set; }
     }
 }
