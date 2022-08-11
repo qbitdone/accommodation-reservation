5afa4d799dc11dd3c356c13f3ab1e3bcc0516085
Kristijan <kculina@protonmail.ch>
2022-08-11 16:11:41 +0200

    Feat: [Z-3] Add ReservationService and ReservationController

---
+++ Staycation/Staycation.Api/Controllers/ReservationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Staycation.Api.Models;
+using Staycation.Api.Services;
+
+namespace Staycation.Api.Controllers
+{
+    [Route("api/reservation")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        public ReservationService _reservationService;
+
+        public ReservationController(ReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+        
+    }
+}
---
+++ Staycation/Staycation.Api/Services/ReservationService.cs
@@ -0,0 +1,31 @@
+﻿using Staycation.Api.DatabaseContext;
+using Staycation.Api.Models;
+
+namespace Staycation.Api.Services
+{
+    public class ReservationService
+    {
+        private AppDbContext _context;
+
+        public ReservationService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddReservation(ReservationViewModel reservation)
+        {
+            var _reservation = new Reservation()
+            {
+                Email = reservation.Email,
+                CheckIn = reservation.CheckIn,
+                CheckOut = reservation.CheckOut,
+                PersonCount = reservation.PersonCount,
+                Confirmed = reservation.Confirmed,
+                AccommodationId = reservation.AccommodationId
+            };
+
+            _context.Add(_reservation);
+            _context.SaveChanges();
+        }
+    }
+}
