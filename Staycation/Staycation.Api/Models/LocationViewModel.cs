c44c980fd7daab55131052e829bf9f641c09a2a0
Kristijan <kculina@protonmail.ch>
2022-08-11 15:42:41 +0200

    Feat: [Z-2] Annotate LocationViewModel model

--- Staycation/Staycation.Api/Models/LocationViewModel.cs
+++ Staycation/Staycation.Api/Models/LocationViewModel.cs
@@ -1,8 +1,14 @@
-﻿namespace Staycation.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Staycation.Api.Models
 {
     public class LocationViewModel
     {
-        public string Name { get; set; }
+        [MaxLength(800)]
+        public string ImageUrl { get; set; }
+        [MaxLength(50)]
         public int PostalCode { get; set; }
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
     }
 }
